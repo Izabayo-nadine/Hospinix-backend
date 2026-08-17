7b1ecab2cfb79b551163a95293e717ea82f7a007
Izabayo-nadine <149108736+Izabayo-nadine@users.noreply.github.com>
2026-08-18 00:05:05 +0200

    *

--- src/main/java/com/hospital/pharmacy/config/SecurityConfig.java
+++ src/main/java/com/hospital/pharmacy/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000"));
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000","https://hospinix-teal.vercel.app"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(Arrays.asList("*"));
                 configuration.setAllowedHeaders(Arrays.asList("*"));
